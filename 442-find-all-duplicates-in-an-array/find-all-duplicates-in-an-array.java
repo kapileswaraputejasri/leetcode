39827409a792c89bdbc0df1980a3faacc3eadfb1
kapileswaraputejasri <kapileswaraputejasri9670@gmail.com>
2026-07-29 14:05:14 +0530

    Time: 3 ms (99.85%) | Memory: 62.1 MB (48.21%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer>ans=new ArrayList<>();
+        int[] freq=new int[nums.length+1];
+        for(int i=0;i<nums.length;i++)
+        {
+            freq[nums[i]]++;
+        }
+        for(int i=0;i<freq.length;i++)
+        {
+            if(freq[i]>1)
+            {
+                ans.add(i);
+            }
+        }return ans;
+    }
+}
