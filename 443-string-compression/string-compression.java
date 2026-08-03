e606b8ccd4c1bb471efa0a7aafff5f71ff47e2bd
kapileswaraputejasri <kapileswaraputejasri9670@gmail.com>
2026-08-03 21:01:47 +0530

    Time: 1 ms (99.90%) | Memory: 45.5 MB (49.61%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+        int read=0;
+        int write=0;
+        int n=chars.length;
+        while(read<n)
+        {
+            char current=chars[read];
+            int count=0;
+            while(read <n && chars[read]==current)
+            {
+                read++;
+                 count++;
+            }
+
+            chars[write]=current;
+            write++;
+
+            if(count>1)
+            {
+                String str=String.valueOf(count);
+                for(char c:str.toCharArray())
+                {
+                    chars[write]=c;
+                    write++;
+                }
+            }
+        }return write;
+    }
+}
