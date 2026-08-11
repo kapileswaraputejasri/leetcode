fcde54209a5278a72437902411305fb8be8da540
kapileswaraputejasri <kapileswaraputejasri9670@gmail.com>
2026-08-11 15:31:31 +0530

    Time: 1 ms (89.32%), Space: 43.1 MB (61.95%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-       int uppercount=0;
-       for(int i=0;i<word.length();i++)
-       {
-        if(Character.isUpperCase(word.charAt(i)))
+        int uppercount=0;
+        for(int i=0;i<word.length();i++)
         {
-            uppercount++;
-        }
-       }return uppercount==0||uppercount==word.length()||(uppercount==1 &&Character.isUpperCase(word.charAt(0)));
+            if(Character.isUpperCase(word.charAt(i)))
+            {
+                uppercount++;
+            }
+        }return uppercount==0||uppercount==word.length()||(uppercount==1 && Character.isUpperCase(word.charAt(0)));
     }
 }
