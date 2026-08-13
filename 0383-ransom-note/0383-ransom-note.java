6717857cf5349b6f4793194aa78a26ba5126676a
kapileswaraputejasri <kapileswaraputejasri9670@gmail.com>
2026-08-13 22:15:00 +0530

    Time: 1 ms (99.95%), Space: 46.2 MB (89.95%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+         int[] counter=new int[128];
+       for(final char ch:magazine.toCharArray())
+       {
+        ++counter[ch];
+       }
+       for(final char ch:ransomNote.toCharArray())
+       {
+        if(--counter[ch]<0)
+        {
+            return false;
+        }
+       }return true;
+    }
+}
