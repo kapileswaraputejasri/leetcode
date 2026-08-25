058b7185477b5f226c0a38220e0825b3814c97f6
kapileswaraputejasri <kapileswaraputejasri9670@gmail.com>
2026-08-25 10:30:51 +0530

    Time: 1 ms (98.47%) | Memory: 42.5 MB (61.53%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0 || x==1)
+        {
+            return x;
+        }
+        int low=1;int high=x;
+        while(low<=high)
+        {
+           int  mid=low+(high-low)/2;
+            if(mid*mid==x)
+            {
+                return mid;
+            }
+            else if((long)mid *mid>(long)x)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }return Math.round(high);
+    }
+}
