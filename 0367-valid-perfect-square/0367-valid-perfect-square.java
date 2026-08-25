4a570afaaeab5701ccbcab3ec81ccbcc1533b2fa
kapileswaraputejasri <kapileswaraputejasri9670@gmail.com>
2026-08-25 10:55:09 +0530

    Time: 0 ms (100%), Space: 42.3 MB (19.94%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low=1;
+        long high=num;
+
+        if(num==1)
+        {
+            return true;
+        }
+        while(low<=high)
+        {
+            long mid=low+(high-low)/2;
+            if( mid*mid==num)
+            {
+                return true;
+            }
+            else if(mid*mid>num)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }return false;
+    }
+}
